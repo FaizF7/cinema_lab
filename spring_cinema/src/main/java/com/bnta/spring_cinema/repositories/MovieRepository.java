066b5d96fc02a7f42c080d3fcb314d7e80e30b1d
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-02 16:13:24 +0000

    Movie class made

---
+++ spring_cinema/src/main/java/com/bnta/spring_cinema/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.bnta.spring_cinema.repositories;
+
+import com.bnta.spring_cinema.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
